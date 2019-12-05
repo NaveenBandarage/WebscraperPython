64f7ee059e946b6f8b230604403ad21e84cea065
thebanterage <thebanterage@gmail.com>
2019-12-06 11:59:17 +1300

    Comments for virtual environment
    Can't run the virtual machine functions on computer, therefore, commenting for later execution.

--- parsedata.py
+++ parsedata.py
@@ -1,1 +1,8 @@
 print("This is the pasedata.py file"
+
+#This is the command to enter a virtual environment
+#python3 -m venv tutorial-env
+
+#This is the command to activate environment
+#source tutorial-env/bin/activate
+
