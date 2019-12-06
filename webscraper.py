2d1e33c89fb767e156711cebf2a00962d2b736b6
thebanterage <thebanterage@gmail.com>
2019-12-06 14:05:10 +1300

    added my twitter to this
    If this works it should print out the information from the page on my twitter account.

--- webscraper.py
+++ webscraper.py
@@ -1,1 +1,13 @@
-print("This is the webscraper.py file"
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://twitter.com/naveenbandarage'
+
+response = requests.get(url, timeout=5)
+
+content = BeatifulSoup(response.content, "html.parser")
+
+print(content)
+
+print("This is the webscraper.py file")
+
