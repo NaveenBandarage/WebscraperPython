6bbed2a911c4f44cacf5b86695714394035a95d6
thebanterage <thebanterage@gmail.com>
2019-12-06 14:25:26 +1300

    Have some doubts on capability
    I have some doubts on whether this webscraper will work with regards to just running it off twitter.

--- webscraper.py
+++ webscraper.py
@@ -9,5 +9,21 @@ content = BeatifulSoup(response.content, "html.parser")
 
 print(content)
 
+#From here I am going to add more code
+
+#unsure if tweet container actually exists. ANd have doubts on accessiation of certain areas need to have a further play around. 
+#Might have to use API. 
+
+for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):  
+    tweetObject = {
+        "author": tweet.find('h2', attrs={"class": "author"}).text.encode('utf-8'),
+        "date": tweet.find('h5', attrs={"class": "dateTime"}).text.encode('utf-8'),
+        "tweet": tweet.find('p', attrs={"class": "content"}).text.encode('utf-8'),
+        "likes": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
+        "shares": tweet.find('p', attrs={"class": "shares"}).text.encode('utf-8')
+    }
+    print tweetObject
+
+
 print("This is the webscraper.py file")
 
